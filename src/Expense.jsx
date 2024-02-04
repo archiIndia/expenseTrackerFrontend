ba2f6e01d0220a10efff7a05bc1aa99d3da4b1fb
archi <archismandaddy1996@gmail.com>
2024-02-04 18:04:53 +0530

    Error in Item

--- src/Expense.jsx
+++ src/Expense.jsx
@@ -14,6 +14,9 @@ import {
   Download,
   ArrowDown,
   Plus,
+  LocateIcon,
+  TargetIcon,
+  ClipboardEdit,
 } from "lucide-react";
 
 import moment from "moment";
@@ -25,12 +28,15 @@ function Expense() {
   const [expenses, setExpenses] = useState([]); //Store all Exp in this
   const [fId, setFId] = useState(null);
   const [isPerformingAnyAction, setIsPerformingAnyAction] = useState(false);
+  const [spendingList, setSpendingList] = useState([
+    { item_name: "", amount: 100 },
+  ]);
 
   const handleCreateExpense = async () => {
     setIsPerformingAnyAction(true);
     try {
       const data = await createExpense(income_val, expense, date);
-      console.log(data);
+
       setExpenses([...expenses, data]);
     } catch (error) {
       console.log("error");
@@ -41,7 +47,7 @@ function Expense() {
   const loadAll = async () => {
     try {
       const data = await getAllExpenses();
-      console.log(data);
+
       setExpenses([...data]);
     } catch (err) {
       console.log("Fault Lines");
@@ -52,33 +58,62 @@ function Expense() {
     setExpense("");
     SetDate("");
   };
+  //Load all Data for One time...
   useEffect(() => {
     loadAll();
   }, []);
   const handleDelete = async (delexpenseId) => {
+    setIsPerformingAnyAction(true);
     await deleteExpense(delexpenseId);
     //Store the Expenses which are not Deleted...
     const tempArray = expenses.filter((exxp) => {
       return exxp._id !== delexpenseId;
     });
+    setIsPerformingAnyAction(false);
     setExpenses(tempArray);
   };
   const handleFetch = async (fetchId) => {
+    setIsPerformingAnyAction(true);
     const fetched = await findOne(fetchId); //get a single Expense from Backend..
+    setIsPerformingAnyAction(false);
     setIncome_val(fetched?.income);
     setExpense(fetched?.expense);
     SetDate(moment(fetched?.date)?.format("YYYY-MM-DD"));
     setFId(fetchId);
   };
   const handleUpdate = async () => {
-   const abc= await updateOne(fId,{dateVal:date,incomeVal:income_val,expenseVal:expense})
+    setIsPerformingAnyAction(true);
+    const abc = await updateOne(fId, {
+      dateVal: date,
+      incomeVal: income_val,
+      expenseVal: expense,
+    });
     handleClear();
-// find the position we need to update...
-const position= expenses.findIndex((ex)=> ex._id=== fId)
-const temp= expenses;
-temp[position]=abc; // Replace the old Value with abc Value at position
-setExpenses(temp);
-
+    // find the position we need to update...
+    const position = expenses.findIndex((ex) => ex._id === fId);
+    const temp = expenses;
+    temp[position] = abc; // Replace the old Value with abc Value at position
+    setExpenses(temp);
+    setIsPerformingAnyAction(false);
+  };
+  const handleAddItem = () => {
+    setSpendingList([...spendingList, { item_name: "", amount: 0 }]);
+  };
+  const removeItem = async (ind) => {
+    // const tempArray = expenses.filter((exxp) => {
+    //   return exxp._id !== ind;
+    // });
+    console.log(ind);
+    const tempArray = spendingList.filter((product, inx) => inx !== ind);
+    setSpendingList(tempArray);
+  };
+  const handleItemnameChange = (item_name, ind) => {
+    console.log("Item:", item_name);
+    console.log("Index:", ind);
+    const temp = spendingList;
+    temp[ind].item_name = item_name; // Replace the old Value with abc Value at position
+  setSpendingList(temp);
+// console.log(temp[ind]);
   };
 
   return (
@@ -136,26 +171,43 @@ setExpenses(temp);
             </span>
           </div>
           <div>
-            <div className={"flex gap-2"}>
-              <div className={"w-2/3"}>
-                <input
-                  type="text"
-                  placeholder="Spend on item"
-                  className="border rounded p-2 w-full outline-none shadow mt-1"
-                />
+            {spendingList.map((product, index) => (
+              <div className={"flex items-center gap-2"} key={index}>
+                <div className={"w-2/3"}>
+                  <input
+                    type="text"
+                    value={product.item_name}
+                    placeholder="Spend on item"
+                    className="border rounded p-2 w-full outline-none shadow mt-1"
+                    onChange={(ev) => {
+                      handleItemnameChange(ev.target.value, index);
+                    }}
+                  />
+                </div>
+                <div>
+                  <input
+                    type="number"
+                    value={product.amount}
+                    placeholder="Amount"
+                    className="border rounded p-2 w-full outline-none shadow mt-1 text-right"
+                  />
+                </div>
+                <div>
+                  <ClipboardEdit
+                    className={"w-4 h-4 "}
+                    onClick={() => {
+                      removeItem(index);
+                    }}
+                  />
+                </div>
               </div>
-              <div>
-                <input
-                  type="number"
-                  placeholder="Amount"
-                  className="border rounded p-2 w-full outline-none shadow mt-1 text-right"
-                />
-              </div>
-            </div>
+            ))}
+
             <div
               className={
                 "text-blue-500 hover:text-blue-600 font-semibold text-sm inline-flex items-center cursor-pointer mt-2 w-full justify-end  "
               }
+              onClick={handleAddItem}
             >
               <span>
                 <Plus className={"w-4 h-4 "} />
@@ -173,13 +225,17 @@ setExpenses(temp);
               <Loader2 className="animate-spin h-5 w-5 text-white" />
             ) : (
               "Save"
-            )}{" "}
+            )}
           </button>
           <button
             onClick={handleUpdate}
             className="text-yellow-600  bg-yellow-50 border uppercase font-bold py-2 px-4 rounded items-center flex justify-center"
           >
-            Update
+            {isPerformingAnyAction ? (
+              <LocateIcon className="animate-spin h-5 w-5 text-white" />
+            ) : (
+              "Update"
+            )}
           </button>
           <button
             onClick={handleClear}
@@ -200,7 +256,7 @@ setExpenses(temp);
             "grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mt-4 gap-2"
           }
         >
-          {expenses.map((exp, index) => (
+          {expenses.map((exp,index) => (
             <div
               key={index}
               className="flex flex-row  w-full rounded shadow-md p-6 border"
@@ -226,7 +282,8 @@ setExpenses(temp);
                       <span className={"text-xs"}>Expense</span>
                       <span
                         className={`w-fit text-md text-right ${
-                          exp.expense > 500
+                          //String Interpolation
+                          exp.expense > 5000
                             ? "text-red-500 rounded bg-red-200 px-1"
                             : ""
                         } `}
@@ -243,18 +300,26 @@ setExpenses(temp);
                   </div>
                 </div>
                 <div className="flex space-x-2">
-                  <Trash2
-                    onClick={() => {
-                      handleDelete(exp._id);
-                    }}
-                    className="cursor-pointer h-5 w-5 text-red-500"
-                  />
-                  <PenLine
-                    onClick={() => {
-                      handleFetch(exp._id);
-                    }}
-                    className="cursor-pointer h-5 w-5 text-green-500"
-                  />
+                  {isPerformingAnyAction ? (
+                    <Loader2 className="animate-spin h-5 w-5 text-white" />
+                  ) : (
+                    <Trash2
+                      onClick={() => {
+                        handleDelete(exp._id);
+                      }}
+                      className="cursor-pointer h-5 w-5 text-red-500"
+                    />
+                  )}
+                  {isPerformingAnyAction ? (
+                    <Loader2 className="animate-spin h-5 w-5 text-white" />
+                  ) : (
+                    <PenLine
+                      onClick={() => {
+                        handleFetch(exp._id);
+                      }}
+                      className="cursor-pointer h-5 w-5 text-green-500"
+                    />
+                  )}
                 </div>
               </div>
             </div>
