c54fa4240a28af5b8d1040f1205fdeaeffaeceba
Surojit Paul <surojit99923@gmail.com>
2024-02-17 14:48:50 +0530

    Shadcn ui added.

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -1,7 +1,14 @@
 import Expense from "./Expense.jsx";
 import SideBar from "./SideBar.jsx";
+import LoginPage from "@/LoginPage.jsx";
+import SignUpPage from "@/SignUpPage.jsx";
 
 const HomePage = () => {
+  const isVerified = false;
+  if (!isVerified) {
+    return <SignUpPage />;
+  }
+
   return (
     <div className={"flex"}>
       <div className={"min-w-60"}>
